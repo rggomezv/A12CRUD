7b19c94754ab6dd559ad14fc3109d9f5deb12ec7
Raul Gomez <rggomezv@hotmail.com>
2023-11-18 02:42:17 -0500

    Layout Created

--- src/app/crud/product-list/product-list.component.ts
+++ src/app/crud/product-list/product-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 }
+];
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
